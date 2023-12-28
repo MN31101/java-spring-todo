f6377fc91a767cf0ea2c53d4c22dd86798ad1281
Mykyta <nktpedko@gmail.com>
2023-12-28 18:28:06 +0200

    create project
    Create project by stert.spring.io

---
+++ src/main/java/com/example/spingtodoapplcation/SpingTodoApplcationApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingtodoapplcation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingTodoApplcationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingTodoApplcationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spingtodoapplcation/SpingTodoApplcationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingtodoapplcation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingTodoApplcationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
